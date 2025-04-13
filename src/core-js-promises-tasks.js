8968e983ce61b4dd89d1daab1deae8ed48d752a9
iKingsman <vladislavkingsman@gmail.com>
2025-04-13 14:18:31 +0300

    feat: add solutions for 2 functions

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -110,8 +110,10 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.all(
+    promises.map((item) => item.then((value) => value).catch(() => null))
+  );
 }
 
 /**
@@ -132,8 +134,12 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((accum, promise) => {
+    return accum.then((result) =>
+      promise.then((value) => result.toString() + value)
+    );
+  });
 }
 
 module.exports = {
